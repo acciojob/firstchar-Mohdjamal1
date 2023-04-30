d841b7298e87fccaf4714c83cc298999cab28568
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-04-30 22:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	if (text === ' ' || text === '') {
+		return '';
+	}
+	else{
+		text.trim(' '); 
+		return text.charAt(0);
+	}
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(text)); 
